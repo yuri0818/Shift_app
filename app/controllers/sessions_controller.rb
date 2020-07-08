3aedb3c42006f48c97fc8b9d75d0e7e43071e33f
yuri0818 <yyyy.0818.0830@gmail.com>
2020-07-08 06:57:24 +0000

    Finish full-fledged-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
+
   def new
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = '認証に失敗しました。'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,16 @@ class UsersController < ApplicationController
   def show
   end
   
+  def new
+    @user = User.new
+  end
   
-  
-  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
 end
